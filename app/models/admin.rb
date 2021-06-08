2513a68238b5923570466d3abb2aeafc655602d4
alexandre <alecarv98@gmail.com>
2021-06-08 10:55:29 -0300

    dominio paynow

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,6 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@paynow\.com\.br\z/ }
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should allow_value('gonzaga@paynow.com.br').for(:email) }  
+  it { should_not allow_value('cartola@hotmail.com').for(:email) }
 end
