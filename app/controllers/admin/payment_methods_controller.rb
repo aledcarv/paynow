b7a7076752623b8091a64e9bcc01170d8dad70fd
alexandre <alecarv98@gmail.com>
2021-06-09 15:56:47 -0300

    WIP admin pode editar meios de pagamento

--- app/controllers/admin/payment_methods_controller.rb
+++ app/controllers/admin/payment_methods_controller.rb
@@ -1,10 +1,11 @@
 class Admin::PaymentMethodsController < Admin::AdminController
+    before_action :set_payment_method, only: %i[show edit update]
+
     def index
         @payment_methods = PaymentMethod.all
     end
 
     def show
-        @payment_method = PaymentMethod.find(params[:id])
     end
 
     def new
@@ -21,9 +22,24 @@ class Admin::PaymentMethodsController < Admin::AdminController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @payment_method.update(payment_method_params)
+            redirect_to [:admin, @payment_method]
+        else
+            render :edit
+        end
+    end
+
     private
 
         def payment_method_params
             params.require(:payment_method).permit(:name, :tax_porcentage, :tax_maximum, :status)
+        end
+
+        def set_payment_method
+            @payment_method = PaymentMethod.find(params[:id])
         end
 end
---
+++ spec/system/admin/admin_edits_payment_method_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe 'admin edits payment method' do
+    it 'successfully' do
+        pay_method = PaymentMethod.create!(name: 'Boleto do banco laranja', tax_porcentage: 10, 
+                                           tax_maximum: 100, status: true)
+
+        admin_login
+        visit root_path
+        click_on 'Meios de pagamento'
+        click_on 'Boleto do banco laranja'
+        click_on 'Editar meio de pagamento'
+
+        fill_in 'Nome', with: 'Boleto do banco roxo'
+        fill_in 'Taxa de cobrança', with: 5
+        fill_in 'Taxa máxima', with: 90
+        select 'Inativo', from: 'Status'
+        click_on 'Editar'
+
+        expect(current_path).to eq(admin_payment_method_path(pay_method))
+        expect(page).to have_content('Boleto do banco roxo')
+        expect(page).to have_content('5,0%')
+        expect(page).to have_content('R$ 90,00')
+        expect(page).to have_content('Inativo')
+    end
+
+    it 'and can not be blank' do
+        pay_method = PaymentMethod.create!(name: 'Boleto do banco laranja', tax_porcentage: 10,
+                                           tax_maximum: 100, status: true)
+
+        admin_login
+        visit root_path
+        click_on 'Meios de pagamento'
+        click_on 'Boleto do banco laranja'
+        click_on 'Editar meio de pagamento'
+
+        click_on 'Editar'
+
+        expect(page).to have_content('não pode ficar em branco', count: 3)
+    end
+
+    it 'and name must be unique' do
+        pay_method = PaymentMethod.create!(name: 'Boleto do banco laranja', tax_porcentage: 10,
+                                           tax_maximum: 100, status: true)
+
+        admin_login
+        visit root_path
+        click_on 'Meios de pagamento'
+        click_on 'Boleto do banco laranja'
+        click_on 'Editar meio de pagamento'
+
+        fill_in 'Nome', with: 'Boleto do banco laranja'
+        fill_in 'Taxa de cobrança', with: 15
+        fill_in 'Taxa máxima', with: 150
+        select 'Ativo', from: 'Status'
+        click_on 'Editar'
+
+        expect(page).to have_content('já está em uso')
+    end
+end
