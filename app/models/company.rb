eef51aa170bd8674308a5fc534427ad6f56bf996
alexandre <alecarv98@gmail.com>
2021-06-13 12:02:47 -0300

    adicionando token em company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,6 @@ class Company < ApplicationRecord
     validates :name, :cnpj, :financial_adress, :financial_email, presence: true
     validates :name, :cnpj, :financial_adress, :financial_email, uniqueness: true
     validates :cnpj, length: { is: 14 }
+
+    has_secure_token, length: 20
 end
